167e04b03bd1805f84a764a228eb0ace0fb53c14
atrizdawiz <andreasostling@tutanota.com>
2024-03-08 21:31:56 +0100

    working dev server config with es2020

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-console.log('Vi är på gång!')
+import express from 'express'
+import { createServer } from 'node:http'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
 
-const index = () => {}
+const app = express()
+const server = createServer(app)
 
-index()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'))
+})
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000')
+})
